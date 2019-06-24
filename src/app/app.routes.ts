62d64d1bb9685406eae00f575daed50127fa695a
Himani <himani.mahto@shipsy.co.in>
2019-06-24 11:51:15 +0530

    Routing and navigation fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,25 +3,16 @@ import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+{   path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+},
 {
     path: 'my-dashboard',
     component: MyDashboardComponent,
-//   resolve: {
-//     data: CategoriesResolver
-// 	}
 },
 {
     path: 'home',
     component: HomeComponent,
-// resolve: {
-//   data: CategoryQuestionsResolver
-// }
 },
-// {
-//     path: 'question/:questionSlug',
-//     component: QuestionAnswersComponent,
-//     resolve: {
-//       data: QuestionAnswersResolver
-//   	}
-// }
 ];
